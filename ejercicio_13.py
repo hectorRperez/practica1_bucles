d6534481b878efb19103b14021ebac4308816084
hector perez <hectorperez1238@gmail.com>
2020-08-02 15:56:52 -0400

    ejercicio 13

---
+++ ejercicio_13.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    
+    while True:
+        
+        palabra = input('Ingrese una palabra: ')
+        if palabra == 'salir':
+            print('Saliendo')
+            break
+        else:
+            print(palabra)
